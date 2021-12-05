5cf366896395ee3641e10d5a9a78957f9e4cfa4f
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-12-05 02:13:00 +0000

    Remove logic from instrument

--- src/instrument.cpp
+++ src/instrument.cpp
@@ -6,42 +6,4 @@
 
 namespace blga {
 
-instrument::instrument(
-    const blga::envelope& envelope,
-    const blga::oscillator& oscillator
-)
-    : d_envelope(envelope)
-    , d_oscillator(oscillator)
-{
-}
-
-auto instrument::note_on(int key, double time) -> void
-{
-    d_notes.emplace_back(key, time, true);
-}
-
-auto instrument::note_off(int key, double time) -> void
-{
-    for (auto& note : d_notes) {
-        if (note.key == key && note.active) {
-            note.active = false;
-            note.toggle_time = time;
-        }
-    }
-}
-
-auto instrument::amplitude(double time) -> double
-{
-    double amp = 0.0;
-    auto count = std::erase_if(d_notes, [&](const auto& note) {
-        if (note.active || time < note.toggle_time + d_envelope.release_time) {
-            amp += d_envelope.amplitude(time, note.toggle_time, note.active) *
-                d_oscillator(blga::note_frequency(note.key), time);
-            return false;
-        }
-        return true; // Delete all notes that are done
-    });
-    return amp;
-}
-
 }
--- src/instrument.h
+++ src/instrument.h
@@ -18,17 +18,8 @@ struct note
 
 struct instrument
 {
-    std::vector<note> d_notes; // Temporary, generalise later
-    blga::envelope    d_envelope;
-    blga::oscillator  d_oscillator;
-
-public:
-    instrument(const blga::envelope& envelope, const blga::oscillator& oscillator);
-
-    auto note_on(int note, double time) -> void;
-    auto note_off(int note, double time) -> void;
-
-    auto amplitude(double time) -> double;
+    blga::envelope   d_envelope;
+    blga::oscillator d_oscillator;
 };
 
 
