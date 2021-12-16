2f0cf757ee5e1341e1d49fc54278fa0d52555041
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-12-16 02:51:22 +0000

    Migrate to Sprocket (#3)
    * Use my `sprocket` library to create a window
    * Use Sprockets event system and game loop to handle keyboard input rather than the windows API.
    * This does not use `on_update`, so could be completely event driven. This wont always be the case, but consider creating a mode in `sprocket` to only update on events rather than spinning in a game loop.

--- src/beluga.m.cpp
+++ src/beluga.m.cpp
@@ -3,6 +3,11 @@
 #include "noise_maker.h"
 #include "components.h"
 
+#include <sprocket/core/game_loop.h>
+#include <sprocket/core/window.h>
+#include <sprocket/core/events.h>
+#include <sprocket/core/input.h>
+
 #include <fmt/format.h>
 
 #include <cmath>
@@ -10,61 +15,87 @@
 #include <numbers>
 #include <optional>
 
-auto is_key_down(char key) -> bool
-{
-    return GetAsyncKeyState(static_cast<unsigned char>(key)) & 0x8000;
-}
+const auto instrument = blga::instrument{
+    .envelope = blga::envelope{
+        .attack_time = 0.01,
+        .decay_time = 0.01,
+        .release_time = 0.3,
+        .start_amplitude = 1.2,
+        .sustain_amplitude = 0.8
+    },
+    .oscillator = [](double frequency, double time) {
+        constexpr auto two_pi = 2.0 * std::numbers::pi;
+        const auto lfo = 0.0 * frequency * std::sin(two_pi * 5.0 * time);
 
-auto main() -> int
-{
-    fmt::print(blga::keyboard_ascii);
-
-    auto kb = blga::instrument{
-        blga::envelope{
-            .attack_time = 0.01,
-            .decay_time = 0.01,
-            .release_time = 0.3,
-            .start_amplitude = 1.2,
-            .sustain_amplitude = 0.8
-        },
-        [](double frequency, double time) {
-            constexpr auto two_pi = 2.0 * std::numbers::pi;
-            const auto lfo = 0.0 * frequency * std::sin(two_pi * 5.0 * time);
-
-            double amp = 0.0;
-            for (double i = 1; i < 10; ++i) {
-                amp += std::sin(two_pi * frequency * i * time + lfo) / i;
-            }
-            return amp / 10;
+        double amp = 0.0;
+        for (double i = 1; i < 10; ++i) {
+            amp += std::sin(two_pi * frequency * i * time + lfo) / i;
         }
-    };
-    auto sound = blga::noise_maker{};
-    sound.add_channel(kb);
+        return amp / 10;
+    }
+};
 
-    std::unordered_map<char, bool> input;
-    const auto is_key_active = [&](char k) {
-        if (auto it = input.find(k); it != input.end()) {
-            return it->second;
-        }
-        return false;
-    };
+constexpr auto keyboard = std::array{
+    spkt::Keyboard::Z,
+    spkt::Keyboard::S,
+    spkt::Keyboard::X,
+    spkt::Keyboard::D,
+    spkt::Keyboard::C,
+    spkt::Keyboard::V,
+    spkt::Keyboard::G,
+    spkt::Keyboard::B,
+    spkt::Keyboard::H,
+    spkt::Keyboard::N,
+    spkt::Keyboard::J,
+    spkt::Keyboard::M,
+    spkt::Keyboard::COMMA,
+    spkt::Keyboard::L,
+    spkt::Keyboard::PERIOD,
+    spkt::Keyboard::SEMI_COLON,
+    spkt::Keyboard::FORWARD_SLASH
+};
 
-    while (!is_key_down('A')) {
-        for (auto [index, key] : blga::enumerate(blga::keyboard)) {
-            int k = index + 15;
-            auto key_down = is_key_down(key);
-            auto active = is_key_active(key);
+class beluga
+{
+    spkt::window* d_window;
 
-            if (key_down && !active) {
-                sound.note_on(k, 0);
-                input[key] = true;
+    blga::noise_maker d_sound;
+
+public:
+    beluga(spkt::window* window)
+        : d_window(window)
+        , d_sound{}
+    {
+        d_sound.add_channel(instrument);
+    }
+
+    void on_update(double dt) {}
+
+    void on_event(spkt::event& event)
+    {
+        if (auto data = event.get_if<spkt::keyboard_pressed_event>()) {
+            if (data->key == spkt::Keyboard::A) {
+                d_window->close();
+                return;
             }
-            else if (!key_down && active) {
-                sound.note_off(k, 0);
-                input[key] = false;
+            // key == musical keyboard, button = computer keyboard
+            for (auto [key, button] : keyboard | blga::enumerate(15)) {
+                if (data->key == button) {
+                    d_sound.note_on(key, 0);
+                }
+            }
+        }
+        else if (auto data = event.get_if<spkt::keyboard_released_event>()) {
+            for (auto [key, button] : keyboard | blga::enumerate(15)) {
+                if (data->key == button) {
+                    d_sound.note_off(key, 0);
+                }
             }
         }
     }
+};
 
-    return 0;
+auto main() -> int
+{
+    return spkt::run_app<beluga>("beluga");
 }
